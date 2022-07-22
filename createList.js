248cbdbc66249f88093be23b495055e0e952195e
Christian Rau <rauchristian@hotmail.com>
2022-07-22 11:59:44 +0200

    .

---
+++ createList.js
@@ -0,0 +1,9 @@
+// create list from contents in folder with images 
+// and save it to a file
+
+// loop over contents of folder
+// create list item with image and text
+// add list item to list
+// save list to file
+
+
